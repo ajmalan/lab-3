a82ccbbc8d774c2d27d30af9f5f1e900b771eb89
ajmalan <89461079+ajmalan@users.noreply.github.com>
2021-09-12 23:06:03 +0000

    Updated

--- Main.java
+++ Main.java
@@ -2,22 +2,20 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-
     Scanner s = new Scanner(System.in);
     System.out.println("Enter a number to count down or up from: ");
     int userNum = s.nextInt();
-    if (userNum > 0) {
-      for (int i = userNum; i >= 0; i--) {
-        System.out.println(i);
+    if (userNum >= 0) {
+      while (userNum >= 0) {
+        System.out.println(userNum);
+        userNum--;
       }
-      System.out.println("Blast off!");
-    } else if (userNum < 0) {
-      for (int i = userNum; i <= -0; i++) {
-        System.out.println(i);
-      }
-      System.out.println("Blast off!");
     } else {
-      System.out.println("Blast Off!");
+      while (userNum <= 0) {
+        System.out.println(userNum);
+        userNum++;
+      }
     }
+    System.out.println("Blast Off!");
   }
 }
